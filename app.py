2e3fedac91f13565f16d619f76dde1e99c43a41e
Dawid <dejw.baran@student.uj.edu.pl>
2026-02-12 01:21:27 +0100

    change next bahavior

--- app.py
+++ app.py
@@ -273,14 +273,12 @@ if map_data["last_clicked"]:
         st.success(f"Poprawnie! {st.session_state.current_object} znajduje się tutaj ✅")
         st.session_state.found_objects[st.session_state.current_object] = correct_coords
         st.session_state.score += 1
-        st.session_state.current_object = None  # przygotowanie do następnego obiektu
 
     else:
         st.error(f"Niepoprawnie! Spróbuj ponownie. Twój błąd: {int(distance)} km")
         folium.Marker(location=correct_coords).add_to(m)
         st.session_state.score += 1
         st.session_state.found_objects[st.session_state.current_object] = correct_coords
-        st.session_state.current_object = None  # przygotowanie do następnego obiektu
 
 
 st.write(f"Twój wynik: {st.session_state.score} / {len(objects)}")
